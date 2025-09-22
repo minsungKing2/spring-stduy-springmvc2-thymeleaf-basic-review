123ae0d3390e22415d3e7251d3b3152312dc19ea
MinSung Jung <minpro22@naver.com>
2025-09-22 11:50:18 +0900

    25.09.22 Thymeleaf 예제 추가

---
+++ src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -0,0 +1,22 @@
+package hello.thymeleaf.basic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//템플릿 조각
+@Controller
+@RequestMapping("/template")
+public class TemplateController {
+
+    @GetMapping("/fragment")
+    public String template() {
+        return "template/fragment/fragmentMain";
+    }
+
+    //템플릿 레이아웃1
+    @GetMapping("/layout")
+    public String layout() {
+        return "template/layout/layoutMain";
+    }
+}
